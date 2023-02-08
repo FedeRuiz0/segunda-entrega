d2c0b6bc319d506c2a3a454179eb29d573427d60
fede ruiz <fedenox0@gmail.com>
2023-02-07 22:32:00 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+
+import mongoose from "mongoose";
+import productsRoutes from "./routes/products.routes.js";
+import cartsRoutes from "./routes/carts.routes.js";
+import viewsRouter from './routes/views.routes.js';
+
+const app = express();
+mongoose.set('strictQuery', true);
+const user = 'kaoh0';
+const dbname = 'cluster0';
+const password = '05EnuLXUjYRUjXcA';
+const uri =  `mongodb+srv://${user}:${password}@cluster0.lh4w9og.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('DB conectado'))
+    .catch(err => console.log(err))
+
+// handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'hbs')
+app.set('views', `${__dirname}/views`)
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+
+// Routes
+app.use('/api/products', productsRoutes);
+app.use('/api/cart', cartsRoutes);
+app.use('/', viewsRouter);
+
+app.listen(8080, () => console.log('Server up in port 8080'))
+
